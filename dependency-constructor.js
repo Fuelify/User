1814ef2249829c722c69cecac5a4bc19e3279dd5
Ethan Minier <ethan@fuelifyyourlife.com>
2021-06-20 19:01:01 +1200

    Created: dependency-constructor script for establishing service and repository module requires
    Centralized script for managing import requirements

---
+++ dependency-constructor.js
@@ -0,0 +1,26 @@
+
+const { createContainer, asClass, asValue } = require('awilix');
+
+// Repositories
+//const UsersRepository = require('./repositories/user-repository');
+
+// Services
+const TokenService = require('./services/token-service');
+
+// Models
+
+const container = createContainer();
+
+function setup() {
+  
+  container.register({
+    //usersRepository: asClass(UsersRepository),
+    
+    tokenService: asClass(TokenService),
+  });
+}
+
+module.exports = {
+  container,
+  setup
+};
