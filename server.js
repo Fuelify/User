dde039b615001a21f79da3e611b9e2c2724b4659
Ethan Minier <ethan@fuelifyyourlife.com>
2021-06-27 10:14:38 +1200

    Added: mock food data to return from /foods/fetch endpoint

--- server.js
+++ server.js
@@ -93,7 +93,68 @@ app.get('/foods/fetch', verifytoken, function(req, res) {
     // Access the provided 'page' and 'limt' query parameters
     let limit = req.query.limit;
 
-    var foods = {}
+    var foods = [
+        {
+            name: 'Lauren Turner',
+            designation: 'Content Writer',
+            userFavorites: 4,
+            bio:
+                'Psychology, science, and art are what helps me to learn the outside world and myself.',
+            age: 24,
+            imgUrl: 'assets/Food1.jpg',
+            location: 'North London'
+        },
+        {
+            name: 'Lori Perez',
+            designation: 'UI Designer ',
+            userFavorites: 8,
+            bio:
+                'Travelling, adventures, extreme sports are also an integral part of me, but I like watching and admiring extreme sports rather than doing it ?',
+            location: 'Leeds',
+            age: 26,
+            imgUrl: 'assets/Food2.jpg',
+        },
+        {
+            name: 'Christine Wallace',
+            designation: 'News Reporter',
+            userFavorites: 2,
+            bio:
+                'Psychology, science, and art are what helps me to learn the outside world and myself.',
+            location: 'Liverpool',
+            age: 23,
+            imgUrl: 'assets/Food3.jpg',
+        },
+        {
+            name: 'Rachel Green',
+            designation: 'Architect',
+            userFavorites: 8,
+            bio:
+                'Psychology, science, and art are what helps me to learn the outside world and myself.',
+            location: 'Nottingham',
+            age: 22,
+            imgUrl: 'assets/Food4.jpg',
+        },
+        {
+            name: 'Emma',
+            designation: 'Software Developer',
+            userFavorites: 3,
+            bio:
+                'Psychology, science, and art are what helps me to learn the outside world and myself.',
+            location: 'Manchester',
+            age: 25,
+            imgUrl: 'assets/Food5.jpg',
+        },
+        {
+            name: 'Kim Wexler',
+            designation: 'Accountant',
+            userFavorites: 5,
+            bio:
+                'Psychology, science, and art are what helps me to learn the outside world and myself.',
+            location: 'Birmingham',
+            age: 30,
+            imgUrl: 'assets/Food6.jpg',
+        },
+    ]
 
     // Retrive another n
     res.status(200).send({"message": "Returning another "+limit.toString()+" food items.", "foods": foods });
