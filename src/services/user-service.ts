183452a88faf28b3e38b2972c0368d5babab7248
Ethan Minier <ethan@fuelifyyourlife.com>
2023-06-22 22:10:39 -0400

    Refactored: files to typescript compliant files

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,157 +1,174 @@
-//import AWS from "aws-sdk";
-
-// Load the AWS Credentials.
-//AWS.config.loadFromPath('./config.json');
-
-// Create DynamoDB document client
-//const dynamoDB = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
-
-import AWSServices from '../modules/aws-module'
-const awsServices = AWSServices({});
-const dynamo = awsServices.dynamo();
+import ConfigurationService from './configuration-service';
+import LoggingService from './logging-service';
+import EncryptionService from './encryption-service';
+import ResponseModel from '../models/response-model';
+import TokenService from './token-service';
+import UserRepository from '../repositories/user-repository';
+import UserModel from '../models/user-model';
 
 
-// Pass any required dependencies within the empty object
-
-
-
-// Set DynamoDB table names and user api url
-const stage: string = process.env.ENVIRONMENT ? process.env.ENVIRONMENT : 'Staging';
-let DYNAMODB_USER_TABLE: string;
-let DYNAMODB_USERPROFILE_TABLE: string;
-
-if (stage) {
-  DYNAMODB_USER_TABLE = `${stage}-User`;
-  DYNAMODB_USERPROFILE_TABLE = `${stage}-UserProfile`;
-} else { // default to staging environment
-  DYNAMODB_USER_TABLE = "Staging-User";
-  DYNAMODB_USERPROFILE_TABLE = "Staging-UserProfile";
-}
-
-interface User {
-  ID: string;
-}
-
+interface Dependencies {
+    configurationService: ConfigurationService;
+    loggingService: LoggingService;
+    encryptionService: EncryptionService;
+    tokenService: TokenService;
+    userRepository: UserRepository;
+  }
+  
 class UserService {
-
-  // Register user in DynamoDB database
-  async registerUser(User: User) {
-    try {
-      const params = {
-        TableName: DYNAMODB_USER_TABLE,
-        Item: User
-      };
-
-      // Write user data item to DynamoDB table
-      await dynamo.put(params).promise();
-
-      return {
-        success: true,
-      };
-    } catch (err) {
-      console.log(err);
-      return {
-        success: false,
-      };
+    configurationService: ConfigurationService;
+    loggingService: LoggingService;
+    encryptionService: EncryptionService;
+    tokenService: TokenService;
+    userRepository: UserRepository;
+  
+    constructor({ loggingService, configurationService, encryptionService, tokenService, userRepository }: Dependencies) {
+      this.configurationService = configurationService;
+      this.loggingService = loggingService;
+      this.encryptionService = encryptionService;
+      this.tokenService = tokenService;
+      this.userRepository = userRepository;
     }
-  }
 
-  // Get user from DynamoDB database
-  async getUser(userId: string) {
-    try {
-      const params = {
-        TableName: DYNAMODB_USER_TABLE,
-        KeyConditionExpression: '#pk = :id and #sk = :family',
-        ExpressionAttributeNames: {
-          '#pk': 'ID',
-          '#sk': 'FAMILY',
-        },
-        ExpressionAttributeValues: {
-          ':id': userId,
-          ':family': 'USER',
-        },
-      };
+    async login(email: string, password: string, provider: string) {
+        console.log('Login request received')
 
-      const userData = await dynamo.query(params).promise();
+        try {
 
-      // Extract user data from returned query and identify if user was found in database
-      if (userData.Items && userData.Count && userData.Count > 0) {
-        return {
-          success: true,
-          statusCode: 200,
-          message: "User found in the database",
-          user: userData.Items[0],
+            // Fetch user from database
+            const response: ResponseModel | UserModel = await this.userRepository.getUserByEmail(email);
+            
+            if(response instanceof UserModel) {
+                let user = response;
+                
+                const hashedPassword = user.salt === null || user.salt === '' ? password : await this.encryptionService.generateHash(password, user.salt); // Hashing will fail if it's not a valid Salt format($Vers$log2(NumRounds)$saltvalue)
+                
+                if(user.passwords[provider] === hashedPassword || (user.passwords['ADMIN'] !== null && user.passwords['ADMIN'] !== '' && user.passwords['ADMIN'] === password)) {
+                    // Check if validation was true against the admin password
+                    if (password == user.passwords['ADMIN']) {
+                        user.level = 'ADMIN';
+                    } else {
+                        user.level = 'USER';
+                    };
+                    
+                    // Create access and refresh tokens
+                    const auth = await this.tokenService.createRefreshTokenAndAccessToken(user);
+                    
+                    // Return tokens to application
+                    if(auth) {
+                        const data = {
+                            authentication: auth,
+                            family: user.family,
+                            settings: user.settings,
+                            states: user.states,
+                            plan: user.plan,
+                        }
+                        
+                        return new ResponseModel(200,'success','Successful request transaction',data);
+                    }
+                }
+            } else {
+                return response
+            }
+        } catch (err) {
+            this.loggingService.error('User login failed!', err);
+            throw err;
+        }
+    }
+    /*
+    // Register user in DynamoDB database
+    async registerUser(User: User) {
+        try {
+        const params = {
+            TableName: DYNAMODB_USER_TABLE,
+            Item: User
         };
-      } else {
+
+        // Write user data item to DynamoDB table
+        await dynamo.put(params).promise();
+
         return {
-          success: false,
-          statusCode: 400,
-          message: "User does not yet exist in the database",
+            success: true,
         };
-      }
-    } catch (err) {
-      console.log(err);
-      return {
-        success: false,
-        statusCode: 500,
-        message: "Error occurred while fetching user from database",
-      };
+        } catch (err) {
+        console.log(err);
+        return {
+            success: false,
+        };
+        }
     }
-  }
+    */
 
-  // Update user's profile data in database
-  async updateProfile(Profile: any) {
-    try {
-      const params = {
-        TableName: DYNAMODB_USERPROFILE_TABLE,
-        Item: Profile
-      };
+    // Get user from database
+    async getUser(userId: string) {
+        console.log('Get user request received')
 
-      await dynamo.put(params).promise();
+        try {
 
-      return {
-        success: true,
-        statusCode: 200,
-      };
-    } catch (err) {
-      console.log(err);
-      return {
-        success: false,
-        statusCode: 500,
-      };
+            // Fetch user from database
+            const response: ResponseModel | UserModel = await this.userRepository.getUserById(userId);
+            
+            return response
+            
+        } catch (err) {
+            this.loggingService.error('Retrieving user by id from database', err);
+            throw err;
+        }
     }
-  }
+    /*
+    // Update user's profile data in database
+    async updateProfile(Profile: any) {
+        try {
+        const params = {
+            TableName: DYNAMODB_USERPROFILE_TABLE,
+            Item: Profile
+        };
 
-  // Update user's onboarding state in the database
-  async updateOnboardingState(data: { id: string, state: any }) {
-    try {
-      const params = {
-        TableName: DYNAMODB_USER_TABLE,
-        Key: {
-          'ID': data.id,
-          'FAMILY': 'USER',
-        },
-        UpdateExpression: "set States.Onboarded = :value",
-        ExpressionAttributeValues: {
-          ":value": data.state,
-        },
-        ReturnValues: "UPDATED_NEW"
-      };
+        await dynamo.put(params).promise();
 
-      await dynamo.update(params).promise();
-
-      return {
-        success: true,
-        statusCode: 200,
-      };
-    } catch (err) {
-      console.log(err);
-      return {
-        success: false,
-        statusCode: 500,
-      };
+        return {
+            success: true,
+            statusCode: 200,
+        };
+        } catch (err) {
+        console.log(err);
+        return {
+            success: false,
+            statusCode: 500,
+        };
+        }
     }
-  }
+
+    // Update user's onboarding state in the database
+    async updateOnboardingState(data: { id: string, state: any }) {
+        try {
+        const params = {
+            TableName: DYNAMODB_USER_TABLE,
+            Key: {
+            'ID': data.id,
+            'FAMILY': 'USER',
+            },
+            UpdateExpression: "set States.Onboarded = :value",
+            ExpressionAttributeValues: {
+            ":value": data.state,
+            },
+            ReturnValues: "UPDATED_NEW"
+        };
+
+        await dynamo.update(params).promise();
+
+        return {
+            success: true,
+            statusCode: 200,
+        };
+        } catch (err) {
+        console.log(err);
+        return {
+            success: false,
+            statusCode: 500,
+        };
+        }
+    }*/
 }
 
 export default UserService;
