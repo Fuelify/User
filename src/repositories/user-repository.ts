98efd2da4146cb5e151856ad5b368370284ca5aa
Ethan Minier <ethan@fuelifyyourlife.com>
2023-11-04 20:54:03 -0400

    Added: user-repository methods for creating user, updating user (scaffold), deleting user, and updating a users password

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -3,14 +3,13 @@ import LoggingService from "../services/logging-service";
 import {AWSServices} from "../modules/aws-module";
 import ResponseModel from "../models/response-model";
 import UserModel from "../models/user-model";
-import { GetCommand, GetCommandInput, GetCommandOutput, QueryCommand, QueryCommandOutput } from "@aws-sdk/lib-dynamodb";
+import { DeleteCommand, DeleteCommandOutput, GetCommand, GetCommandInput, GetCommandOutput, PutCommand, PutCommandOutput, QueryCommand, QueryCommandOutput, UpdateCommand, UpdateCommandInput, UpdateCommandOutput } from "@aws-sdk/lib-dynamodb";
 
 interface Dependencies {
     configurationService: ConfigurationService;
     loggingService: LoggingService;
     responseModel: typeof ResponseModel;
     awsModule: AWSServices;
-    userModel: typeof UserModel;
   }
   
 class UserRepository {
@@ -18,15 +17,60 @@ class UserRepository {
     loggingService: LoggingService;
     responseModel: typeof ResponseModel;
     aws: AWSServices;
-    userModel: typeof UserModel;
   
-    constructor({ loggingService, configurationService, responseModel, awsModule, userModel }: Dependencies) {
+    constructor({ loggingService, configurationService, responseModel, awsModule }: Dependencies) {
       this.configurationService = configurationService;
       this.loggingService = loggingService;
       this.responseModel = responseModel;
       this.aws = awsModule;
-      this.userModel = userModel;
     }
+
+    async createUser(user: UserModel) {
+      
+      try {
+        const params = {
+            TableName: this.configurationService.FUELIFY_USER_DYNAMODB_TABLENAME,
+            Item: user.toDatabase()
+        };
+        
+        const command = new PutCommand(params);
+
+        const response: PutCommandOutput = await this.aws.dynamo().send(command);
+
+        return new ResponseModel(200, "success", "User successfully created in the database!", user.toSanitizedJson());
+        
+      } catch (err: any) {
+          this.loggingService.error("Error occurred while attempting to create user in database",err)
+          return new ResponseModel(500, "error", "Error occurred while attempting to create user in database",err.toString());
+      }
+
+    }
+
+    async updateUser(user: UserModel) {
+    }
+
+    async deleteUser(user: UserModel) {
+      try {
+        const params = {
+          TableName: this.configurationService.FUELIFY_USER_DYNAMODB_TABLENAME,
+          Key: {
+            ID : user.id,
+            FAMILY: user.family
+          }
+        };
+
+        const command = new DeleteCommand(params);
+
+        const response: DeleteCommandOutput = await this.aws.dynamo().send(command);
+
+        return new ResponseModel(200, "success", "User successfully deleted from the database!");
+        
+      } catch (err: any) {
+        this.loggingService.error("Error occurred while deleting user from database",err)
+        return new ResponseModel(500, "error", "Error occurred while deleting user from database",err.toString());
+      }
+    }
+    
     
     async getUserByEmail(email: string) {
         try {
@@ -53,7 +97,7 @@ class UserRepository {
             const response: QueryCommandOutput = await this.aws.dynamo().send(command);
             
             // Identify if user was found in database
-            if (response.Items) {
+            if (response.Items && response.Items.length > 0) {
                 // Extract user data from returned query
                 return UserModel.fromJson(response.Items[0]);
           } else {
@@ -66,22 +110,13 @@ class UserRepository {
 
     }
     
-    async getUserById(userId: string) {
+    async getUserById(userId: string, family: string = 'USER') {
         try {
             const params: GetCommandInput = {
                 TableName: this.configurationService.FUELIFY_USER_DYNAMODB_TABLENAME,
-                /*KeyConditionExpression: '#pk = :id and #sk = :family',
-                ExpressionAttributeNames: {
-                  '#pk': 'ID',
-                  '#sk': 'FAMILY',
-                },
-                ExpressionAttributeValues: {
-                  ':id': userId,
-                  ':family': 'USER',
-                },*/
                 Key: {
                   'ID': userId,
-                  'FAMILY': 'USER'
+                  'FAMILY': family
                 }
             };
 
@@ -102,6 +137,35 @@ class UserRepository {
       }
 
     }
+
+    async updateUserPassword(user: UserModel) {
+
+      try {
+          const params: UpdateCommandInput = {
+              TableName: this.configurationService.FUELIFY_USER_DYNAMODB_TABLENAME,
+              Key: {
+                  'ID' : user.id,
+                  'FAMILY': user.family
+              },
+              UpdateExpression: "SET Passwords = :Passwords, Salt = :Salt",
+              ExpressionAttributeValues: {
+                  ':Passwords': user.passwords,
+                  ':Salt': user.salt
+              }
+          };
+
+          const command = new UpdateCommand(params);
+
+          const response: UpdateCommandOutput = await this.aws.dynamo().send(command);
+
+          return new ResponseModel(200, "success", "User password updated in the database!");
+          
+      } catch (err: any) {
+          this.loggingService.error("Error occurred while attempting to update user password in database",err)
+          return new ResponseModel(500, "error", "Error occurred while attempting to update user password in database",err.toString());
+      }
+  }
+
 }
 
 export default UserRepository;
